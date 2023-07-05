d971aff477c97f3a80e078c5bf1935f217b3efc3
Thiago Gomes <thi23gm@gmail.com>
2023-07-05 12:28:58 -0300

    banner and about ok

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
+import {motion} from "framer-motion";
+import { fadeIn } from "../variants";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section id='about' className='section'>
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+          <motion.div variants={fadeIn('right', 0.5)} initial='hidden' whileInView={'show'} viewport={{once:false, amount: 0.7}} className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend bg-top"></motion.div>
+          <div className="flex-1">
+            <motion.h2 variants={fadeIn('left', 0.3)} initial='hidden' whileInView={'show'} viewport={{once:false, amount: 0.7}} className='h2 text-accent'>About Me</motion.h2>
+            <motion.h3 variants={fadeIn('left', 0.4)} initial='hidden' whileInView={'show'} viewport={{once:false, amount: 0.7}} className='h3 mb-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </motion.h3>
+          <motion.p variants={fadeIn('left', 0.5)} initial='hidden' whileInView={'show'} viewport={{once:false, amount: 0.7}} className='mb-6'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe vero vel minus perspiciatis, asperiores optio? Veniam nisi, neque, cum placeat tempore, adipisci exercitationem rem nihil nulla aliquam accusamus delectus a?</motion.p >
+          <motion.div variants={fadeIn('left', 0.5)} initial='hidden' whileInView={'show'} viewport={{once:false, amount: 0.7}} className='flex gap-x-8 items-center mt-4'>
+            <button className='btn btn-lg'>Contact Me</button>
+            <a href="#" className='text-gradient btn-link'>
+              MyPortifilio
+            </a>
+          </motion.div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default About;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import {BiClinic, BiHomeAlt, BiUser} from 'react-icons/bi';
+import {BsClipboard2Data, BsBriefcase, BsChatSquareText} from 'react-icons/bs';
+import {Link} from 'react-scroll';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return ( 
+    <nav className="fixed botton-2 lg:bottom-8 w-full overflow-hidden z-50">
+      <div className="container mx-auto">
+        <div className="w-full bg-black/20 h-[96px] backdrop-blur-2x1 rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-white/50 items-center">
+          <Link to='home' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center' activeClass='actives' smooth={true} spy={true} offset={-200}>
+            <BiHomeAlt />
+          </Link>
+          <Link to='about' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center' activeClass='actives' smooth={true} spy={true}>
+            <BiUser />
+          </Link>
+          <Link to='services' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center' activeClass='actives' smooth={true} spy={true}>
+            <BsClipboard2Data />
+          </Link>
+          <Link to='work' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center' activeClass='actives' smooth={true} spy={true}>
+            <BsBriefcase />
+          </Link>
+          <Link to='contact' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center' activeClass='actives' smooth={true} spy={true}>
+            <BsChatSquareText />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
